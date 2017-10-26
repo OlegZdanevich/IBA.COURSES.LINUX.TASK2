861a40caeb8391afa805665b6fe689dafa741daf
Oleg Zdanevich <ya.zdanevich@yandex.ru>
2017-10-26 18:46:40 +0300

    Final version

--- ibaTask2.pl
+++ ibaTask2.pl
@@ -6,4 +6,8 @@ print "\n";
 print "Enter id of process which you whant to kill\n";
 my $myid=<STDIN>;
 $myid=$myid+0;
-system("pstree -p $myid| perl -ne \'print \"\$_\\n\" foreach /\\((\\d+)\\)/g;\'");
+killProcess($myid);
+
+sub killProcess (myid){
+	system("pstree -p $myid| perl -ne \'kill -9, \$_ foreach /\\((\\d+)\\)/g;\'");
+}
